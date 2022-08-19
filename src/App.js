4ff8c08384504575a7f50f39c46d8dd817836a81
Muhammad Yousuf <yousuf.hanif550@gmail.com>
2022-08-19 23:57:22 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/layouts/Navbar";
 import "./App.css";
+import UserItem from "./components/users/UserItem";
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <Navbar title='GitHub Finder' />
+        <Navbar />
+        <UserItem/>
       </div>
     );
   }
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class UserItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: "1",
+      login: "mojombo",
+      avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+      html_url: "https://github.com/mojombo",
+    };
+  }
+
+  render() {
+    return (<div>
+        
+    </div>);
+  }
+}
+
+export default UserItem;
